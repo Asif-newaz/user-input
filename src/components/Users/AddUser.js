1fb6e417815c1e313369c3a952c7c78239775588
Asif-newaz <asif.newaz0093@gmail.com>
2023-04-01 03:56:31 +0600

    working sign in form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Components
 import Button from "../UI/Button";
@@ -7,17 +7,29 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [EnteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    console.log(enteredUsername, EnteredAge);
+  };
+
+  const userChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label>Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={userChangeHandler} />
         <label>Age (Years)</label>
-        <input id="age" type="number" />
-        <Button>Add User</Button>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
